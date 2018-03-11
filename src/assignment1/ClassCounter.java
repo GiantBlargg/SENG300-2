a89a358fb91a4233b8df0979f4c8ca79c8f51fb7
Ryan pang <Ryan@lucriment.com>
2018-03-10 19:45:39 -0700

    Prints the file and it's contents

--- src/assignment1/ClassCounter.java
+++ src/assignment1/ClassCounter.java
@@ -1,7 +1,9 @@
 package assignment1;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ClassCounter {
 
@@ -17,14 +19,18 @@ public class ClassCounter {
         dir = new File(path);
     }
     
-    public void parseDirectory() {
+    public void parseDirectory() throws IOException {
         if(dir.exists()) {
             File[] files = dir.listFiles();
-            for (File file : files) {
+            for (File file : files) {                                   // Iterates through all the files in the base directory
                 if (file.isFile()) {
                     System.out.println("File " + file.getName());
+                    fr = new FileReader(file);
+                    char a[] = new char[5000];                          // Will contain all characters within the file.
+                    fr.read(a);                                         // Reads the characters to the Array.
+                    System.out.println(new String(a));
                   } else if (file.isDirectory()) {
-                    System.out.println("Directory " + file.getName());
+                    System.out.println("Directory " + file.getName());  
                   }
             }
         }
@@ -36,7 +42,12 @@ public class ClassCounter {
     
     public static void main(String args[]) {
         ClassCounter cc = new ClassCounter(args[0],args[1]);
-        cc.parseDirectory();
+        try {
+            cc.parseDirectory();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
 }
