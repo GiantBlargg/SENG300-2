3aa33c6bd7aa054710f4eeaba656ca49f0dd3cfd
Ryan pang <Ryan@lucriment.com>
2018-03-13 15:15:20 -0600

    Class counter displays references and declarations through ReferenceCounter.

--- src/assignment1/ClassCounter.java
+++ src/assignment1/ClassCounter.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import assignment1.ReferenceCounter.counts;
+
 public class ClassCounter {
 
     private String BASEDIR;
@@ -22,35 +24,28 @@ public class ClassCounter {
     
     public void parseBaseDirectory() throws IOException {
         if(dir.exists()) {
+            ReferenceCounter rf = new ReferenceCounter(BASEDIR);
             File[] files = dir.listFiles();
             for (File file : files) {                                   // Iterates through all the files in the base directory
                 if (file.isFile()) {
-                    System.out.println("File " + file.getName());
-                    
+                    fr = new FileReader(file);
                     char a[] = new char[5000];                          // Will contain all characters within the file.
                     fr.read(a);                                         // Reads the characters to the Array.
-                    String contents = new String(a);
-                    while(contents.indexOf(CLASSTYPE)!=-1) {
-                        REFCOUNT++;
-                        contents = contents.substring(contents.indexOf(CLASSTYPE)+CLASSTYPE.length(),contents.length()-1);
-                    }
-                    System.out.println(REFCOUNT);
-                  } else if (file.isDirectory()) {
-                    System.out.println("Directory " + file.getName());  
-                  }
+                    rf.setSource(a);
+                    counts c = rf.count(CLASSTYPE);
+                    DECCOUNT += c.Declarations;
+                    REFCOUNT += c.References;
+                }
             }
         }
-    }
-    
-    
-    public static void parseFile(File file) {
-        
     }
     
     public static void main(String args[]) {
         ClassCounter cc = new ClassCounter(args[0],args[1]);
         try {
             cc.parseBaseDirectory();
+            System.out.println(cc.CLASSTYPE + ". Declarations found: " + cc.DECCOUNT + "; references found: " + cc.REFCOUNT + ".");
+           
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
