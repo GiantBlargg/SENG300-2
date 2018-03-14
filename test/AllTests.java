c74091a07935078af5b5592c4bbe43f30101ef88
Daniel Doran <ddoran3@gmail.com>
2018-03-13 21:55:55 -0600

    BASEDIR is in suite

---
+++ test/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestFull.class })
+public class AllTests {
+	public static String BASEDIR = "";
+}
--- test/TestFull.java
+++ test/TestFull.java
@@ -1,33 +1,30 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import assignment1.DirParser;
 
 public class TestFull {
-     
-    private static String BASEDIR = "";
-    private static String CLASSTYPE = "NoClass";
-    
-    /*****************************************************************************
-     *      
-     *  Test for a directory with no declarations or references to the classType.
-     *  
-     *****************************************************************************/
+
+	private static String CLASSTYPE = "NoClass";
+
+	/*****************************************************************************
+	 * 
+	 * Test for a directory with no declarations or references to the classType.
+	 * 
+	 *****************************************************************************/
 	@Test
 	public void test() {
-	    DirParser counter = new DirParser(BASEDIR, CLASSTYPE);
-	    try {
-            counter.parseBaseDirectory();
-        } catch (IOException e) {
-            fail("IOException");
-        }
-	    assertEquals("NoClass. Declarations found: 0; references found: 0.", counter.getCount());
-    }
+		DirParser counter = new DirParser(AllTests.BASEDIR, CLASSTYPE);
+		try {
+			counter.parseBaseDirectory();
+		} catch (IOException e) {
+			fail("IOException");
+		}
+		assertEquals("NoClass. Declarations found: 0; references found: 0.", counter.getCount());
+	}
 
 }
